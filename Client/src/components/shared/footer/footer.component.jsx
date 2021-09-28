b0176c71427ff5d5529341d823021c6f7ca995cc
Nick-Kh <many_sided@hotmail.com>
2021-09-28 23:54:17 +0300

    upd

---
+++ Client/src/components/shared/footer/footer.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      My Footer
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Client/src/components/shared/header/header.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      My Header
+    </header>
+  );
+}
+
+export default Header;
